16d40f60322d31be42430e4db06076f07a1cf1f1
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2016-07-19 15:51:57 -0500

    all cards show that have images. if no image no show

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-
+import config from '../config/environment';
 export default Ember.Route.extend({
     model() {
       return fetch(`http://tiy-brewery-proxy.herokuapp.com/v2/breweries?key=${config.apiKey}&established=2015`)
